0c94d65d4649635d05de230e5dca813e9bcbc524
焦丹杨 <1154206436@qq.com>
2025-08-30 12:42:50 +0800

    测试用例提交

--- test/disk_tree_test.py
+++ test/disk_tree_test.py
@@ -19,16 +19,152 @@ def test_count(t:BTree):
         node2 = node2.get_right_node()
     print(count)
 
-def test_tree():
-    info = BTreeInfo("my_tree2",-1,1,False,[IntValue])
+# 普通增删改查
+def test_tree1():
+    info = BTreeInfo("my_tree1",-1,1,False,[IntValue,StrValue])
     BTree.create_btree(info,True)
-    t = BTree.open_btree("my_tree2")
-    for i in range(0,60):
-        t.insert(generate_row([i]))
-    t.show()
+    t = BTree.open_btree("my_tree1")
+    # print(t.search(generate_row([1])).get_row_i(0))
+    for i in range(2000):
+        t.insert(generate_row([i, "hello"]))
+    for i in range(200,500):
+        t.delete(generate_row([i]))
+
+    for i in range(700,800):
+        t.delete(generate_row([i]))
+
+    for i in range(1000,1200):
+        t.delete(generate_row([i]))
     test_count(t)
     t.container.flush()
     t.container.close()
 
+# 增删改查2
+def test_tree2():
+    info = BTreeInfo("my_tree2",-1,1,False,[IntValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+                                            ]
+                     )
+    BTree.create_btree(info,True)
+    t = BTree.open_btree("my_tree2")
+    # print(t.search(generate_row([1])).get_row_i(0))
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none()
+                               ]))
 
-test_tree()
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               "hello","hello","hello","hello","hello","hello"
+                               ]))
+
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none()
+                               ]))
+
+    for i in range(300,800):
+        t.delete(generate_row([i]))
+    test_count(t)
+    t.container.flush()
+    t.container.close()
+
+# 增删改查3 长字段修改
+def test_tree3():
+    info = BTreeInfo("my_tree3",-1,1,False,[IntValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+        ,StrValue,StrValue,StrValue,StrValue,StrValue,StrValue
+                                            ]
+                     )
+    BTree.create_btree(info,True)
+    t = BTree.open_btree("my_tree3")
+    # print(t.search(generate_row([1])).get_row_i(0))
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               ]))
+
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               "hello","hello","hello","hello","hello","hello",
+                               ]))
+
+    for i in range(1000):
+        t.insert(generate_row([i,
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),StrValue.none(),
+                               ]))
+
+    for i in range(300,800):
+        t.delete(generate_row([i]))
+    test_count(t)
+    t.container.flush()
+    t.container.close()
+
+test_tree3()
